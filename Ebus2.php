43c50242380620ada43cfc072044804db3163ee0
118374766 <118374766@BISB79VL.bis.ad.ucc.ie>
2019-02-28 10:55:18 +0000

    17

--- Ebus2.php
+++ Ebus2.php
@@ -1,29 +1,64 @@
-<form id="Basket" action="Ebus2.php" method="post">
-    <table class="shopping-cart">
-        <thead>
-            <tr>
-                <th scope="col">Item</th>
-                <th scope="col">Qty</th>
-                <th scope="col">Price</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-    </table>
-    <p id="sub-total">
-        <b>Sub Total</b>: <span id="stotal"></span>
-    </p>
-    <ul id="shopping-cart-actions">
-        <li>
-            <input type="submit" name="update" id="update-cart" class="btn" value="Update Cart" />
-        </li>
-        <li>
-            <input type="submit" name="delete" id="empty-cart" class="btn" value="Empty Cart" />
-        </li>
-        <li>
-            <a href="Ebus1.html" class="btn">Continue Shopping</a>
-        </li>
-        <li>
-            <a href="" class="btn">Go To Checkout</a>
-        </li>
-    </ul>
-</form>
+<?php
+session_start();
+$fullNameValue = "";
+$totalValue2 = "";
+/*
+ * Create a session variable for the mobile number
+ */
+$totalValue = $_POST['txtTotal'];
+$_SESSION['txtName'] = $fullNameValue;
+$_SESSION['txtTotal'] = $totalValue2;
+
+/**
+ * Allocate the mobile number session variable to a text box
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>eBusiness 2</title>
+    </head>
+    
+    <body>
+        <div class="form">
+            <form name="Details" method="post" action="Ebus3.php">
+                <center>
+                    <table cellspacing="10">
+                        <tr>
+                            <td><b></b></td>
+                            <td><b>Enter in your details below</b></td>
+                        </tr>
+                        <tr>
+                            <td>Name</td>
+                            <td><input type="text" id="txtName" name="txtName" value="" /></td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Phone Number</td>
+                            <td><input type="text" id="txtNum" name="txtNum" value="" /></td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Password</td>
+                            <td><input type="text" id="txtPassword" name="txtPassword" value="" /></td>
+                        </tr>
+                        
+                        <tr>
+                            
+                            <td><input type="hidden" id="txtTotal" name="txtTotal" value="<?php echo $totalValue;?>" /></td>
+                        </tr>
+                    </table>
+                </center>
+                
+                <center>
+                    
+                    <input type="submit" name="btnContinue" id="btnContinue" onclick="" value="Continue"/>
+                </center>
+            </form>
+        </div>
+
+    </body>
+   
+</html>
